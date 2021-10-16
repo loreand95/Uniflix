fc93ba7519d47973528cb0dd8bc0a4c6615bf7fb
Lorenzo Andreoli <34028703+loreand95@users.noreply.github.com>
2021-10-16 23:50:25 +0200

    add webapp

---
+++ services/payservice/src/main/java/it/uniflix/payservice/model/User.java
@@ -0,0 +1,73 @@
+package it.uniflix.payservice.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+@XmlRootElement(name="User")
+public class User {
+	
+	private String id;
+	
+	private String email;
+	
+	private String password;
+	
+	private String token;
+	
+	private String fullname;
+
+
+
+	public User() {}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getToken() {
+		return token;
+	}
+
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+
+	public String getFullname() {
+		return fullname;
+	}
+
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+}
---
+++ services/userservice/src/main/java/it/uniflix/userservice/model/Order.java
@@ -0,0 +1,65 @@
+package it.uniflix.userservice.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_NULL)
+@XmlRootElement(name="Order")
+public class Order {
+
+	private int id;
+	
+	private String movieId;
+	
+	private String purchaseDate;
+	
+	private double price;
+	
+	
+	public Order() {}
+
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getMovieId() {
+		return movieId;
+	}
+
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+
+	public String getPurchaseDate() {
+		return purchaseDate;
+	}
+
+
+	public void setPurchaseDate(String purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
