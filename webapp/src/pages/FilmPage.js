fa8f62bc2a24aac3fa27ac948dd8d0db45340933
Lorenzo Andreoli <34028703+loreand95@users.noreply.github.com>
2021-10-27 09:57:12 +0200

    add pay dialog and validation

--- webapp/src/pages/FilmPage.js
+++ webapp/src/pages/FilmPage.js
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import BaseLayout from "./BaseLayout";
-import { useParams } from 'react-router-dom';
-import YouTube from 'react-youtube';
-import { Button, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { getFilmById } from '../api/rest/shopService';
-import { getActorsFetch } from '../api/rest/filmService';
+import { Container, Typography } from "@mui/material";
 import Skeleton from '@mui/material/Skeleton';
-import UserReviews from '../components/UserReviews';
+import * as React from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+import { getActorsFetch } from '../api/rest/filmService';
+import { getFilmById } from '../api/rest/shopService';
 import ActorScrollCards from '../components/ActorScrollCards';
+import BuyButton from '../components/BuyButton';
+import UserReviews from '../components/UserReviews';
+import BaseLayout from "./BaseLayout";
 
 export default function FilmPage({ data }) {
 
@@ -24,71 +24,61 @@ export default function FilmPage({ data }) {
         }
     }, [id]);
 
-    const opts = {
-        height: '720',
-        width: '60%',
-        playerVars: {
-            autoplay: 0,
-            showinfo: 0
-        },
-    };
-
     return (
         <BaseLayout>
-            <div style={{ textAlign: 'center', marginTop: '15px' }}>
-                {
-                    film ?
-                        <div style={{ minHeight: '750px' }}>
-                            <YouTube videoId="Ulws6xyu14A" opts={opts} />
-                        </div>
-                        :
-                        <div style={{
-                            maxWidth: '60%',
-                            margin: '0 auto'
-                        }}>
-                            <Skeleton height={750} />
-                        </div>
+            <Container maxWidth='xl'>
+                <div style={{ textAlign: 'center' }}>
+                    {
+                        film ?
+                            <img
+                                alt={film.title}
+                                style={{ width: '100%', height: '400px', objectFit: "cover" }}
+                                src={'https://image.tmdb.org/t/p/original' + film.backdropPath}
+                            />
+                            :
+                            <div style={{
+                                maxWidth: '60%',
+                                margin: '0 auto'
+                            }}>
+                                <Skeleton height={300} />
+                            </div>
 
-                }
+                    }
+                    <div style={{
+                        maxWidth: '60%',
+                        margin: '0 auto',
+                    }}>
+                        <Typography
+                            variant='h3'>
+                            {film ? film.title : <Skeleton />}
+                        </Typography>
+                        <Typography
+                            variant='h6'>
+                            {film ? `${film.author} - ${film.year} - ${film.duration} min` : <Skeleton />}
+                        </Typography>
+                        {film && <BuyButton film={film} />}
+                        <div style={{
+                            textAlign: 'justify'
+                        }}>
+                            <Typography
+                                variant='h4'>
+                                Description
+                            </Typography>
+                            <Typography
+                                variant='body'>
+                                {film ? film.overview : <Skeleton />}
+                            </Typography>
+                        </div>
+                    </div>
+                </div>
                 <div style={{
                     maxWidth: '60%',
                     margin: '0 auto',
                 }}>
-                    <Typography
-                        variant='h3'>
-                        {film ? film.title : <Skeleton />}
-                    </Typography>
-                    <Typography
-                        variant='h6'>
-                        {film ? `${film.author} - ${film.year} - ${film.duration} min` : <Skeleton />}
-                    </Typography>
-                    {film &&
-                        <Button
-                            sx={{ mt: 5, mb: 5, width: '200px' }}
-                            variant='contained'>
-                            BUY $ {film.price}
-                        </Button>}
-                    <div style={{
-                        textAlign: 'justify'
-                    }}>
-                        <Typography
-                            variant='h4'>
-                            Description
-                        </Typography>
-                        <Typography
-                            variant='body'>
-                            {film ? film.overview : <Skeleton />}
-                        </Typography>
-                    </div>
+                    {film && actors && <ActorScrollCards actors={actors} />}
+                    {film && <UserReviews movie={film} />}
                 </div>
-            </div>
-            <div style={{
-                    maxWidth: '60%',
-                    margin: '0 auto',
-                }}>
-            {film && actors && <ActorScrollCards actors={actors}/> }
-            {film && <UserReviews movie={film}/> }
-            </div>
+            </Container>
         </BaseLayout>
     );
 }
