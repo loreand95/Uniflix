5c6068ea8d1a6d931509bc9095e4cbbacd213a20
Lorenzo Andreoli <34028703+loreand95@users.noreply.github.com>
2021-11-17 15:14:12 +0100

    added cors filter and singleton to Actor Service

--- services/actorservice/src/main/java/it/uniflix/actorservice/service/MongoConnection.java
+++ services/actorservice/src/main/java/it/uniflix/actorservice/service/MongoConnection.java
@@ -2,34 +2,52 @@ package it.uniflix.actorservice.service;
 
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
-
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-
 public class MongoConnection {
 	
 	private static final String CONNECTION_STRING = "mongodb+srv://steflo:pizza11@cluster0.xmrck.mongodb.net/Uniflix?retryWrites=true&w=majority";
 	private static final String DATABASE_NAME = "Uniflix";
-	
-	public static MongoDatabase getDatabase() {
 
+	private static MongoConnection mongoConnection;
+	
+	private static MongoClient mongoClient;
+	
+	private MongoConnection() {
+		
 		ConnectionString connectionString = new ConnectionString(CONNECTION_STRING);
 		
-        // create codec registry for POJOs
+        // Create codec registry for POJOs
         CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
                 fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-        
+       
 		MongoClientSettings settings = MongoClientSettings.builder()
 				.applyConnectionString(connectionString)
 				.codecRegistry(pojoCodecRegistry)
 				.build();
-		MongoClient mongoClient = MongoClients.create(settings);
+		
+		mongoClient = MongoClients.create(settings);
+		
+	}
+	
+	public static synchronized MongoConnection getInstance() {
+		
+		if(mongoConnection == null) {
+			mongoConnection = new MongoConnection();
+		}
+		
+		return mongoConnection;
+	}
+	
+	public MongoDatabase getDatabase() {
 		return mongoClient.getDatabase(DATABASE_NAME);	
 	}
 }
+
+
