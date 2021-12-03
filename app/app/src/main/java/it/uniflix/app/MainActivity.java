1798055bf59219b716943072a980ae990e0e2342
Stefano Florio <31825478+StephanXV@users.noreply.github.com>
2021-12-03 13:13:06 +0100

    Review Activity Init

--- app/app/src/main/java/it/uniflix/app/MainActivity.java
+++ app/app/src/main/java/it/uniflix/app/MainActivity.java
@@ -2,7 +2,10 @@ package it.uniflix.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.reviews:
+                Intent intent = new Intent(getApplicationContext(), ReviewsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 }
---
+++ app/app/src/main/java/it/uniflix/app/ReviewsActivity.java
@@ -0,0 +1,18 @@
+package it.uniflix.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+    }
+
+}
